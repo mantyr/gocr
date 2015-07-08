63d4e77b6b7b5d8336853639d5523490f5e5752d
Ted Price <ted.price@gmail.com>
2015-07-07 22:49:16 -0500

    Adding the math library

---
+++ math.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+  "math/rand"
+  "math"
+)
+
+// Random weight between -0.2 and 0.2
+func Rand() float64 {
+  return rand.NormFloat64() * 0.4 - 0.2
+}
+
+// Mean squared error
+func MSE(errors []float64) float64 {
+  sum := 0.0
+
+  for _, i := range errors {
+    sum = sum + float64(i) * float64(i)
+  }
+  return sum / float64(len(errors))
+}
+
+
+func Sum(array []float64) float64 {
+  sum := 0.0
+
+  for _, i := range array {
+    sum = sum + float64(i)
+  }
+  return sum
+}
+
+func Sigmoid(x float64) float64 {
+  return 1 / ( 1 + math.Pow(math.E, -x))
+}
